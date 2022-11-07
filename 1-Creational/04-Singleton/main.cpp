079a281a49e3e381eec7fb23b581a1a6f71e81ff
sohail70 <sohail.e.nia@gmail.com>
2022-11-07 23:59:01 +0330

    Add thread-safe singleton

--- 1-Creational/04-Singleton/main.cpp
+++ 1-Creational/04-Singleton/main.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
-
-
-
+#include<mutex>
+#include<thread>
+#include<chrono>
 
 class Singleton{
     public:
         static Singleton* getInstance(std::string value_)
         {
+            std::lock_guard<std::mutex> lock(mutex);
             if(instance == nullptr)
                 instance = new Singleton(value_);
             
@@ -24,16 +25,34 @@ class Singleton{
     private:
         Singleton(std::string value_):value(value_){}
         static Singleton* instance;
+        static std::mutex mutex;
         std::string value;
 };
 Singleton* Singleton::instance = nullptr;
+std::mutex Singleton::mutex;
 
 
+void func1()
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    auto soheil = Singleton::getInstance("Soheil");
+    soheil->func();
+}
+
+
+void func2()
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    auto saman = Singleton::getInstance("Saman");
+    saman->func();
+}
 
 int main()
 {
-    auto name = Singleton::getInstance("Soheil");
-    name->func();
-    auto name2 = Singleton::getInstance("Saman");
-    name->func();
+    auto t1 = std::thread(&func1);
+    auto t2 = std::thread(&func2);
+
+    t1.join();
+    t2.join();
+
 }
